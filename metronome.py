627be8ffd8c3c4173a1a972226e841986204fb2e
Igor Kaper <igk1001@gmail.com>
2020-11-27 15:23:18 -0500

    adding tone matching

--- metronome.py
+++ metronome.py
@@ -3,8 +3,12 @@ import sys
 import time
 from bluepy.btle import UUID, Peripheral,DefaultDelegate
 import threading
+from queue import LifoQueue
 from gpiozero.tones import Tone
 from time import sleep
+#import os
+#os.environ['SDL_AUDIODRIVER'] = 'dsp'
+
 from pygame import mixer
 
 button_service_uuid = "00001812-0000-1000-8000-00805f9b34fb"
@@ -12,27 +16,74 @@ button_chararcteristics_uuid    = "00002a4d-0000-1000-8000-00805f9b34fb"
 
 lock = threading.Lock()
 
+generator_queue = LifoQueue()
+ble_receiver_queue = LifoQueue()
+
+results = []
+
+#Admin APIs to manage a service
+class Admin:
+    pass
+
+class Move:
+    pass
+
+#defines desired metronome sequence
+# Ex. press left hand twice, right hand once,  left hand twice, etc
+class Sequence:
+    pass
+
 #matches bits and tones
-class Matcher:
+#t0, t1, t2
+class Matcher(threading.Thread):
     def __init__(self):
+        super(Matcher, self).__init__(name="Matcher")
         self.dd = 1
+
+    def run(self):
+        print ("creating thread {}".format (threading.currentThread().getName()))
+        self.process()
+
+    def process(self):
+        while True:
+            #print ("matching gqueue={}, blequeue={}".format (generator_queue.qsize(), ble_receiver_queue.qsize()))
+            if ble_receiver_queue.empty == False:
+                gitem = generator_queue.pop()
+                ritem = ble_receiver_queue.pop()
+                diff = gitem.ts-ritem.ts
+                print ("difference {}, gqueue={}, blequeue={}".format (diff, generator_queue.qsize(), ble_receiver_queue.qsize()))
+                sleep (0.1)
+
 
 #generates sounds as per defined order
 class BitGenerator(threading.Thread):
+    class GItem:
+        def __init__(self, ts):
+            self.ts = ts
+
     def __init__(self, rate):
-        super(BitGenerator, self).__init__()
+        super(BitGenerator, self).__init__(name="Generator")
         self.rate=rate
         mixer.init()
-        sound = mixer.Sound('sounds/bottle_pop_2.wav')
+        self.sound = mixer.Sound('sounds/bottle_pop_2.wav')
 
     def run(self):
-        print ("creating thread {} for {}".format (threading.currentThread().getName(), self.device))
+        print ("creating thread {}".format (threading.currentThread().getName()))
         self.generate()
     
     def generate(self):
-        
+        while True:
+            item = self.GItem(int(time.time() * 1000))
+            generator_queue.put(item)
+            self.sound.play()
+            #sleep (self.rate)
 
 class BLEProcessor(threading.Thread):
+    class BLEItem:
+        def __init__(self, ts, device):
+            self.ts = ts
+            self.device = device
+
     def __init__(self, device):
         super(BLEProcessor, self).__init__(name=device)
         self.device=device
@@ -79,12 +130,20 @@ class MyDelegate(DefaultDelegate):
     def __init__(self, params):
         DefaultDelegate.__init__(self)
         self.device=params
+        #hack to suppress duplicate notifications
+        self.count = 0
       
     #func is caled on notifications
     def handleNotification(self, cHandle, data):
-         #print ("Notification from Handle: 0x" + format(cHandle,'02X') + " Value: "+ format(ord(data[0])))
-         print ( int(time.time() * 1000), " Notification from Device:" + self.device + ", Handle: 0x" + format(cHandle,'02X') + " Value: "+ str(data))
-      
+        ts = int(time.time() * 1000)
+        print (str(ts) + " Notification from Device:" + self.device + ", Handle: 0x" + format(cHandle,'02X') + " Count" + str(self.count))
+        if self.count == 0:
+            ble_receiver_queue.put(BLEProcessor.BLEItem(ts, self.device))
+            self.count = self.count + 1
+        else:
+         if self.count > 0:
+            self.count = 0
+        
 
 if len(sys.argv) != 2:
   print ("Fatal, must pass device address:", sys.argv[0], "<device address="">")
@@ -95,6 +154,17 @@ thread_list = []
 
 for d in sys.argv[1].split(','):
     devices.append(d.strip())
+
+matcher = Matcher()
+matcher.setDaemon(True)
+matcher.start()
+thread_list.append(matcher)
+
+generator = BitGenerator(1)
+generator.setDaemon(True)
+generator.start()
+thread_list.append(generator)
+
 
 for d in devices:
     thread = BLEProcessor(d)
